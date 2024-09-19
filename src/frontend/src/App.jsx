41d2c8c34b819ba29f08123605a7ab0891ba5d8e
C4P5 <caprioli.santiago@gmail.com>
2024-09-19 10:07:04 -0700

    pinche canister cabronazo

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,9 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProductPage from "./Components/Products/ProductPage";
+import MainPage from "./Components/Pages/MainPage";
+import Cart from "./Components/Cart/Cart";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+    children: [
+      {
+        index: true,
+        element: <ProductPage />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <main>
-      <div>Hello!</div>
-    </main>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App;
